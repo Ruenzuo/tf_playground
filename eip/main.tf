41c660416d6636c57a7080fe9d5887da47528ad3
Renzo Crisostomo <renzo@crisostomo.me>
2018-05-03 23:15:07 +0200

    Add Terraform configuration file
    * Use AMI created by Packer
    * Create EC2 instance

---
+++ eip/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "packer_created" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["packer-created-*"]
+  }
+
+  owners = ["self"]
+}
+
+output "aws_ami_packer_created_id" {
+  value = "${data.aws_ami.packer_created.image_id}"
+}
+
+resource "aws_instance" "tf_playground_instance" {
+  ami           = "${data.aws_ami.packer_created.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "tf_playground"
+  }
+}
+
+output "aws_instance_tf_playground_instance_id" {
+  value = "${aws_instance.tf_playground_instance.id}"
+}
