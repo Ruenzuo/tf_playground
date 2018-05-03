3b845ae5133008825a91b2849492de8689069faf
Renzo Crisostomo <renzo@crisostomo.me>
2018-05-03 23:50:13 +0200

    Destroy everything!

--- eip_ds/main.tf
+++ eip_ds/main.tf
@@ -2,14 +2,3 @@ provider "aws" {
   region  = "us-west-2"
   version = "1.17.1"
 }
-
-data "aws_eip" "tf_playground_instance_eip" {
-  filter {
-    name   = "tag:Name"
-    values = ["tf_playground_instance_eip"]
-  }
-}
-
-output "aws_eip_tf_playground_instance_eip" {
-  value = "${data.aws_eip.tf_playground_instance_eip.public_ip}"
-}
